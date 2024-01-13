97c5faf6a12ec5ecae37910303d1ac455bcccd6a
Miles Koehnemann <70864211+CCMiles32@users.noreply.github.com>
2024-01-13 21:57:00 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  return {
+    scope: {},
+    bindToController: {
+      user: '='
+    },
+    controller: function() {},
+    controllerAs: 'vm',
+    templateUrl: 'member.html'
+  };
+}
